0fe7a731bcd4060842a60352b73a0657b6011e14
Emeenent14 <chukwuemekaoguejiofor14@example.com>
2025-07-26 09:09:00 +0100

    fonts init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 import { SlidersVertical, RefreshCw, Users, ShieldUser } from 'lucide-react';
 
+
 function App() {
   return (
     <div className="flex flex-col md:flex-row w-screen min-h-screen">
     {/* Left */}
-    <div className="flex items-center justify-center w-full md:w-[35%] bg-white">
+    <div className="flex items-center justify-center w-full md:w-[35%] bg-white ">
       <img
         src="/Image 3 (1).png"
-        alt="Left Visual"
+        alt="logo xio"
         className="w-[90%] max-w-[400px] object-contain"
       />
     </div>
@@ -18,32 +19,32 @@ function App() {
         <div className="w-full min-h-screen py-6 px-8">
           {/* Hero Section */}
           <section className="py-6">
-            <p className="inline-block bg-red-200 px-4 py-1 rounded-3xl mb-4 border border-[#BB304F]">
+            <p className="inline-block bg-red-100 px-4 py-1 rounded-3xl my-3 border border-[#BB304F]">
               Self Hosted
             </p>
-            <h1 className="mb-3 font-mono font-medium text-4xl leading-tight">
+            <h1 className="mb-3 font-mono font-medium text-4xl leading-tight py-2">
               AI support for your complete team
             </h1>
-            <p className="text-base leading-relaxed text-gray-700 font-jakarta font-normal tracking-[0.13px]">
+            <p className="text-base leading-relaxed text-gray-800 font-jakarta font-normal tracking-[0.13px]">
               Xio offers hosted and self-hosted versions enabling your whole team
               to harness the power of AI, ensuring multi-user access and privacy.
             </p>
           </section>
 
           {/* Feature Row 1 */}
-          <section className="flex flex-col md:flex-row py-6">
+          <section className="flex flex-col md:flex-row py-3">
             <div className="flex-1 py-[18px] px-[20px] border-t border-[#BB304F]">
               <SlidersVertical className="text-[#BB304F] w-6 h-7 mb-3" />
-              <h2 className="text-xl font-semibold mb-2">White-Labeled</h2>
-              <p className="font-jakarta font-normal tracking-[0.13px]">
+              <h2 className="text-xl font-[18px] mb-2">White-Labeled</h2>
+              <p className="font-jakarta font-normal tracking-[0.13px] text-gray-700">
                 Xio can be readily customized and white-labeled to match your
                 company's branding and identity.
               </p>
             </div>
             <div className="flex-1 py-[18px] px-[20px] border-t border-l border-[#BB304F]">
               <RefreshCw className="text-[#BB304F] w-6 h-7 mb-3" />
-              <h2 className="text-xl font-semibold mb-2">Ecosystem</h2>
-              <p className="font-jakarta font-normal tracking-[0.13px]">
+              <h2 className="text-xl font-[18px]  mb-2">Ecosystem</h2>
+              <p className="font-jakarta font-normal tracking-[0.13px] text-gray-700">
                 Adapt features and designs to fit your organization's workflow
                 without breaking security.
               </p>
@@ -51,8 +52,8 @@ function App() {
           </section>
 
           {/* Center Image */}
-          <section className="flex justify-center py-12">
-            <div className="w-[300px] h-[300px]">
+          <section className="flex justify-center py-1 ">
+            <div className="w-[300px] h-[300px] ">
               <img
                 src="/Frame 1000001711.png"
                 alt="Hexagon"
@@ -62,19 +63,19 @@ function App() {
           </section>
 
           {/* Feature Row 2 */}
-          <section className="flex flex-col md:flex-row py-6">
+          <section className="flex flex-col md:flex-row py-3">
             <div className="flex-1 py-[18px] px-[20px] border-b border-[#BB304F]">
               <Users className="text-[#BB304F] w-6 h-7 mb-3" />
-              <h2 className="text-xl font-semibold mb-2">Multi-user</h2>
-              <p className="font-jakarta font-normal tracking-[0.13px]">
+              <h2 className="text-xl font-[18px]  mb-2">Multi-user</h2>
+              <p className="font-jakarta font-normal tracking-[0.13px] text-gray-700">
                 Xio supports multiple users on the same server, ensuring complete 
                 isolation between tenants.
               </p>
             </div>
             <div className="flex-1 py-[18px] px-[20px] border-b border-l border-[#BB304F]">
               <ShieldUser className="text-[#BB304F] w-6 h-7 mb-3" />
-              <h2 className="text-xl font-semibold mb-2">Admin Control</h2>
-              <p className="font-jakarta font-normal tracking-[0.13px]">
+              <h2 className="text-xl font-[18px] mb-2">Admin Control</h2>
+              <p className="font-jakarta font-normal tracking-[0.13px] text-gray-700">
                 Fine-grained admin controls allow you to regulate what your 
                 users can access and perform.
               </p>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@fontsource/plus-jakarta-sans/400.css';
+import '@fontsource/plus-jakarta-sans/500.css'; 
+import '@fontsource/plus-jakarta-sans/600.css'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
